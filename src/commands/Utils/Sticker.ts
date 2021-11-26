6930e893fcb4aa962387616681f13856dd10f349
Ayakajanul <91958060+Ayakajanul@users.noreply.github.com>
2021-11-26 03:07:31 -0800

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -80,8 +80,8 @@ export default class Command extends BaseCommand {
 					})();
 					return {
 						categories,
-						pack: pack[1] || "🌟 Here you go ",
-						author: pack[2] || "Chitoge 🌟",
+						pack: pack[1] || "HERE YOU GO DARLING💖 ",
+						author: pack[2] || "LUMINE 💃",
 						quality,
 						type: StickerTypes[
 							parsedArgs.flags.includes("--crop") ||
