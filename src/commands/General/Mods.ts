9eb073f302e483fc1e4d56257f4bf6ef146491a6
Ayakajanul <91958060+Ayakajanul@users.noreply.github.com>
2021-11-26 17:46:33 -0800

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -20,11 +20,11 @@ export default class Command extends BaseCommand {
         let text = '🍥 *Moderators* 🍥\n\n'
         filteredMap.forEach(
             (user, index) =>
-                (text += `#${index + 1}\n🌟 *Username: ${
+                (text += `#${index + 1}\n💃 *Username: ${
                     user.notify || user.vname || user.name || 'null'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n🌟 LUMINE 🌟 `
+        text += `\n💃 HU-TAO 💃 `
         return void M.reply(text)
     }
 }
