350db78322df961a1791342c941f41365201133a
Ayakajanul <91958060+Ayakajanul@users.noreply.github.com>
2021-11-28 17:38:51 -0800

    Create myowner.ts

---
+++ src/commands/General/myowner.ts
@@ -0,0 +1,35 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "myowner",
+			description: "SHOWS THE GITHUB PROFILE OF THE OWNER",
+			category: "general",
+			usage: `${client.config.prefix}myowner`,
+			baseXp: 10,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			"https://c.tenor.com/LoG04blAZPgAAAPo/emilia-re-zero.mp4";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: `GO CHECK MY MASTER'S PROFILE💖💃
+                   https://github.com/Ayakajanul. \n`,
+			}
+		);
+	};
+}
