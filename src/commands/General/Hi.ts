06c139656c4778f3e746ff80f4b5aab419b4aeac
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2021-12-21 23:58:08 +0300

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -1,1 +1,35 @@
+/** @format */
 
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "hi",
+			description: "Generally used to check if bot is Up",
+			category: "general",
+			usage: `${client.config.prefix}hi`,
+			baseXp: 10,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			"https://c.tenor.com/KeHteFqTRFsAAAPo/hu-tao.mp4";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: `Hello my Chan,I am DREADED a WhatsApp bot created by my MASTER Fortunne, could you mind telling me your name too? Merry Christmas and Happy New year 🎂
+				Anyway use something from *${this.client.config.prefix}help* list if you want anything. \n`,
+			}
+		);
+	};
+}
