4804224e138fe434a97965631c2542a9db4de891
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2021-12-22 00:04:51 +0300

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -17,14 +17,14 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '🍥 *Moderators* 🍥\n\n'
+        let text = '👻 *My OWNER contact* 👻\n\n'
         filteredMap.forEach(
             (user, index) =>
-                (text += `#${index + 1}\n💃 *Username: ${
+                (text += `#${index + 1}\n👾 *Username: ${
                     user.notify || user.vname || user.name || 'null'
-                }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
+                }*\n👾 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n💃 HU-TAO 💃 `
+        text += `\n💃 DREADED💃 `
         return void M.reply(text)
     }
 }
