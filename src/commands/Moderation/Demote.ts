9c75f876a201e81a9e6b3e2418d5fb2eca922841
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2021-12-22 00:57:07 +0300

    Update Demote.ts

--- src/commands/Moderation/Demote.ts
+++ src/commands/Moderation/Demote.ts
@@ -23,10 +23,10 @@ export default class Command extends BaseCommand {
         M.mentioned.forEach(async (user) => {
             const usr = this.client.contacts[user]
             const username = usr.notify || usr.vname || usr.name || user.split('@')[0]
-            if (!M.groupMetadata?.admins?.includes(user)) M.reply(`✖ Skipped *${username}* as they're not an admin`)
+            if (!M.groupMetadata?.admins?.includes(user)) M.reply(`✖Sir you on drugs? I have Skipped *${username}* as they're not an admin`)
             else if (user !== this.client.user.jid) {
                 await this.client.groupDemoteAdmin(M.from, [user])
-                M.reply(`➰ Successfully Demoted *${username}*`)
+                M.reply(`➰Sir I have Successfully Demoted *${username}*`)
             }
         })
     }
