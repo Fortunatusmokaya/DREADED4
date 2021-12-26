bbfb162073c07bb11c44cbd59d7ab7399eb44c52
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2021-12-26 22:46:46 +0300

    Update InviteLink.ts

--- src/commands/General/InviteLink.ts
+++ src/commands/General/InviteLink.ts
@@ -8,7 +8,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'invitelink',
-            aliases: ['invite', 'linkgc'],
+            aliases: ['invite', 'linkgc', 'link'],
             description: 'Get the group invite link',
             category: 'general',
             usage: `${client.config.prefix}invite`,
@@ -29,7 +29,7 @@ export default class Command extends BaseCommand {
                 `*Invite link:* https://chat.whatsapp.com/${code}`,
                 MessageType.text
             )
-            return void M.reply('Check your inbox now!Have sent the group link!')
+            return void M.reply('Check your inbox now!I have just sent the group link!')
         } else {
             return void M.reply(
                 `Command not enabled by the admin.\nUse *${this.client.config.prefix}act invitelink* to enable it`
